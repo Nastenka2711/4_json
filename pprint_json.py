c02098da62f77bd54b165c3cedc8080d20506fc6
Nastenka2711 <Nastasya.saurova@gmail.com>
2016-10-04 14:57:32 +0400

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -2,12 +2,14 @@ import json
 
 
 def load_data(filepath):
-    pass
+    list_info_alcos = json.load(filepath)
+    for list_info_alco in list_info_alcos:
+        pretty_print_json(list_info_alco)
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, skipkeys = True, ensure_ascii = False, sort_keys = False, indent = 4))
 
 
 if __name__ == '__main__':
-    pass
+    load_data(open('vodka.json','r'))
